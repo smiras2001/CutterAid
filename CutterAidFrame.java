73a474efec7a2afa3921200a4f3b088fd0627407
LabUser <smiras@wgu.edu>
2024-03-22 21:43:25 -0400

    Added more labels and spinners

--- CutterAidFrame.java
+++ CutterAidFrame.java
@@ -5,51 +5,108 @@ import java.awt.GridBagConstraints;
 import java.awt.event.ActionListener;
 public class CutterAidFrame extends JFrame {
     public CutterAidFrame() {
+        initializeUI();
+    }
+
+    private void initializeUI() {
+        configureFrame();
+        addMainPanel();
+    }
+
+    private void configureFrame() {
         setTitle("CutterAid");
         setSize(850, 550);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
-        // Main Panel
+    }
+
+    private void addMainPanel() {
+        JPanel mainPanel = createMainPanel();
+        addPaperSize(mainPanel);
+        addFinalSize(mainPanel);
+        addUpness(mainPanel);
+        addMidSpace(mainPanel);
+        getContentPane().add(mainPanel);
+    }
+
+    private JPanel createMainPanel() {
         JPanel mainPanel = new JPanel();
         mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
-        mainPanel.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
-        // Paper size label panel
-        JPanel initPanel = new JPanel();
-        initPanel.setLayout(new BoxLayout(initPanel, BoxLayout.Y_AXIS));
-        JLabel initLabel = new JLabel("Paper Size");
-        Font initLabelFont = initLabel.getFont();
-        initLabel.setFont(initLabelFont.deriveFont(20f));
-        initLabel.setAlignmentX(Component.LEFT_ALIGNMENT);
-        initPanel.add(initLabel);
-        initPanel.add(Box.createVerticalStrut(10));
-        mainPanel.add(initPanel);
-        // Paper size label/spinner panel
-        JPanel firstLabelSpinnerPanel = new JPanel();
-        firstLabelSpinnerPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
-        firstLabelSpinnerPanel.setAlignmentX(Component.LEFT_ALIGNMENT);
-        // Paper size width label/spinner
-        JLabel firstLabel = new JLabel("Width:");
-        JSpinner firstSpinner = new JSpinner(new SpinnerNumberModel(0,0,26,1));
-        firstLabelSpinnerPanel.add(firstLabel);
-        firstLabelSpinnerPanel.add(firstSpinner);
-        firstLabelSpinnerPanel.add(Box.createHorizontalStrut(10));
-        // Paper size length label/spinner
-        JLabel secondLabel = new JLabel("Length");
-        JSpinner secondSpinner = new JSpinner(new SpinnerNumberModel(0,0,26,1));
-        firstLabelSpinnerPanel.add(secondLabel);
-        firstLabelSpinnerPanel.add(secondSpinner);
-        mainPanel.add(firstLabelSpinnerPanel);
-        //Final size label
-        JPanel finalPanel = new JPanel();
-        finalPanel.setLayout(new BoxLayout(finalPanel, BoxLayout.Y_AXIS));
-        JLabel finalLabel = new JLabel("Final size");
-        Font finalLabelFont = finalLabel.getFont();
-        finalLabel.setFont(finalLabelFont.deriveFont(20f));
-        finalLabel.setAlignmentX(Component.LEFT_ALIGNMENT);
-        mainPanel.add(finalLabel);
+        mainPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        return mainPanel;
+    }
+
+    private void addPaperSize(JPanel mainPanel) {
+        JLabel firstLabel = createMainLabel("Paper Size");
+        mainPanel.add(firstLabel);
         mainPanel.add(Box.createVerticalStrut(1));
-        getContentPane().add(mainPanel);
 
+        JPanel firstLabelSpinnerPanel = createLabelSpinnerPanel("Width:", 0,0, 26,1);
+        JPanel secondLabelSpinnerPanel = createLabelSpinnerPanel("Length:", 0, 0,26, 1);
+        JPanel paperSizePanel = new JPanel();
+        paperSizePanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+        paperSizePanel.setAlignmentX(Component.LEFT_ALIGNMENT);
+        paperSizePanel.add(firstLabelSpinnerPanel);
+        paperSizePanel.add(secondLabelSpinnerPanel);
+        mainPanel.add(paperSizePanel);
+    }
 
+    private void addFinalSize (JPanel mainPanel) {
+        JLabel secondLabel = createMainLabel("Final Size");
+        mainPanel.add(secondLabel);
+        mainPanel.add(Box.createVerticalStrut(1));
+
+        JPanel firstLabelSpinnerPanel = createLabelSpinnerPanel("Width:", 0,0, 26,1);
+        JPanel secondLabelSpinnerPanel = createLabelSpinnerPanel("Length:", 0, 0,26, 1);
+        JPanel paperSizePanel = new JPanel();
+        paperSizePanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+        paperSizePanel.setAlignmentX(Component.LEFT_ALIGNMENT);
+        paperSizePanel.add(firstLabelSpinnerPanel);
+        paperSizePanel.add(secondLabelSpinnerPanel);
+        mainPanel.add(paperSizePanel);
+    }
+
+    private void addUpness (JPanel mainPanel) {
+        JLabel thirdLabel = createMainLabel("Upness");
+        mainPanel.add(thirdLabel);
+        mainPanel.add(Box.createVerticalStrut(1));
+
+        JPanel thirdLabelSpinnerPanel = createLabelSpinnerPanel("Up:", 0, 0, 26,1);
+        JPanel finalSizePanel = new JPanel();
+        finalSizePanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+        finalSizePanel.setAlignmentX(Component.LEFT_ALIGNMENT);
+        finalSizePanel.add(thirdLabelSpinnerPanel);
+        mainPanel.add(finalSizePanel);
+    }
+
+    private void addMidSpace (JPanel mainPanel) {
+        JLabel fourthLabel = createMainLabel("Mid Space");
+        mainPanel.add(fourthLabel);
+        mainPanel.add(Box.createVerticalStrut(1));
+
+        JPanel fourthLabelSpinnerPanel = createLabelSpinnerPanel("Width:", 0,0, 26,.125);
+        JPanel fifthLabelSpinnerPanel = createLabelSpinnerPanel("Length:", 0, 0,26, .125);
+        JPanel midSpacePanel = new JPanel();
+        midSpacePanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+        midSpacePanel.setAlignmentX(Component.LEFT_ALIGNMENT);
+        midSpacePanel.add(fourthLabelSpinnerPanel);
+        midSpacePanel.add(fifthLabelSpinnerPanel);
+        mainPanel.add(midSpacePanel);
+    }
+
+    private JLabel createMainLabel(String label) {
+        JLabel mainLabel = new JLabel(label);
+        Font mainLabelFont = mainLabel.getFont();
+        mainLabel.setFont(mainLabelFont.deriveFont(20f));
+        mainLabel.setAlignmentX(Component.LEFT_ALIGNMENT);
+        return mainLabel;
+    }
+    private JPanel createLabelSpinnerPanel(String label, double initialValue,double min, double max, double stepSize) {
+        JPanel panel = new JPanel();
+        JLabel labelText = new JLabel(label);
+        JSpinner spinner = new JSpinner(new SpinnerNumberModel(initialValue, min, max, stepSize));
+        panel.add(labelText);
+        panel.add(spinner);
+        return panel;
     }
 }
